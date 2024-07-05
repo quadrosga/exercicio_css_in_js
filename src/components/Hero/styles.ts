3a78215372ee6386c2825e28e410812ed5c7da90
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-07-05 16:40:53 -0300

    aplicando Styled Components ao Hero

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,4 +1,6 @@
-.form {
+import styled from 'styled-components'
+
+export const Form = styled.form`
   height: 360px;
   width: 100%;
   background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
@@ -6,9 +8,8 @@
   position: relative;
   display: flex;
   align-items: center;
-}
 
-.form::before {
+  &::before {
   position: absolute;
   top: 0;
   left: 0;
@@ -19,23 +20,21 @@
   opacity: 0.7;
 }
 
-.form div {
-  position: relative;
-  color: #eee;
-}
-
-.heroTitle {
-  font-family: Gloock, serif;
-  font-size: 48px;
-}
-
-@media (max-width: 768px) {
+  @media (max-width: 768px) {
   .form {
     height: auto;
     padding: 24px 0;
   }
+`
+export const Container = styled.div`
+  position: relative;
+  color: #eee;
+`
+export const HeroTitle = styled.h2`
+  font-family: Gloock, serif;
+  font-size: 48px;
 
-  .heroTitle {
+  @media (max-width: 768px) {
     font-size: 32px;
   }
-}
+`
