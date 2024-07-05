384f31e258936e2a936d0332ee001658c1715059
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-07-05 15:58:32 -0300

    aplicando tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
 import './global.css'
+import temaMain from './components/themes/main'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaMain}>
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
